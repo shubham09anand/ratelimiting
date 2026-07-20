24f0a0108723777a0340aaa6990924edc108c24c
shubham09anand <shubhamsheelam@gmail.com>
2026-07-20 15:55:14 +0530

    added port 8080

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/server/health", (req, res) => {
      })
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 console.log(PORT);
 
 app.listen(PORT, () => {
